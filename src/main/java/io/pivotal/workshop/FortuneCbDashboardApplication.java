a06c1fd3aaaf159cf4d16b25cba4fec334749771
Andrew Ripka <aripka@pivotal.io>
2016-11-15 21:29:14 -0600

    add circuit breaker dashboard configuration

--- src/main/java/io/pivotal/workshop/FortuneCbDashboardApplication.java
+++ src/main/java/io/pivotal/workshop/FortuneCbDashboardApplication.java
@@ -2,10 +2,21 @@ package io.pivotal.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@Controller
 public class FortuneCbDashboardApplication {
 
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FortuneCbDashboardApplication.class, args);
 	}
